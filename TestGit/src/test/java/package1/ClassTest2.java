7ed7b5e7dbc9d036c32644ec48986d2b2e3a7c18
Victor <Victor@192.168.0.85>
2020-01-16 22:49:22 +0200

    I add two classes

---
+++ TestGit/src/test/java/package1/ClassTest2.java
@@ -0,0 +1,10 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class ClassTest2 {
+	@Test
+	public void meth1() {
+	System.out.println("Hello World");
+	}
+}
---
+++ TestGit/src/test/java/package1/ClassTest3.java
@@ -0,0 +1,10 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class ClassTest3 {
+	@Test
+	public void meth1() {
+	System.out.println("Hello");
+	}
+}
